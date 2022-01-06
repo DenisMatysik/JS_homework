51f1e588243ed650e69b72cde65a9363e616a6b1
DenisMatysik <92157822+DenisMatysik@users.noreply.github.com>
2022-01-06 17:26:41 +0300

    Add files via upload

--- Preza17/script.js
+++ Preza17/script.js
@@ -1,35 +1,49 @@
 // Preza 17 setevie zaprosi
+// Task1
+// const url = 'https://test-todoist.herokuapp.com/api/tasks'
 
-
-// const someHandler = (xhr)=>{
-//     if (xhr.status === 200){
-//         console.log(JSON.parse(xhr.body)[0]);
+// async function getTask () {
+//     const response = await fetch(url)
+//     const data = await response.json()
+//     for (let i=0;i<data.length; i++){
+//     let newDiv = document.createElement("div");
+//     newDiv.innerHTML = `Task №${i+1} ${data[i].description}`;
+//     document.body.appendChild(newDiv)  
+//     console.log(data)}
 //     }
-// }
+//     getTask()
 
+// Task2
 
-// let oXHR = new XMLHttpRequest();
-// oXHR.open("GET", "" ,true)
-// oXHR.addEventListener("readystatechange", someHandler)
-// oXHR.send();
+const url = 'https://jsonplaceholder.typicode.com/users'
 
-const requestUrl = "https://test-todoist.herokuapp.com/api/tasks";
+async function getUsers () {
+    console.log("start")
+    let logMass = ["Bret","Karianne","Elwyn.Skiles","Moriah.Stanton"]
+    const masLog = await fetch(url)
+    let logInfo = await masLog.json()
+    
+    for (let i=0;i<logInfo.length;i++) {
+        let userNameMass = logInfo[i].username;
+        console.log(userNameMass);
+        for (let j=0; j<logMass.length;j++) {
+            if ( logMass[j] === userNameMass){
+                console.log("Takoi polzovatek est")
+            }
+            else {console.log("kakoito chert")}
+        }
+        }
 
-const xhr = new XMLHttpRequest();
-
-    xhr.open("GET", requestUrl)
-
-xhr.onload = () =>{
-    if (xhr.status >=400) {
-        console.error(xhr.response)
-    }
-    else {
-    console.log(JSON.parse(xhr.response))
-    }
+    //     for( let j=0; j<logMass;j++){
+    //         if( logMass[j] === userNameMass ) {
+    //             console.log("taloi login est")
+    //         }
+    //         else{
+    //             console.log("takogo logina net")
+    //         }
+            
+    console.log(logInfo)
 }
 
-xhr.onerror = () =>{
-    console.log(xhr.response)
-}
 
-xhr.send()
+getUsers()
